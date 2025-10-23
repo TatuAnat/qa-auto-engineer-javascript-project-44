673a602fd16372be293f1f018925093e6c548238
TatuAnat <tatiana.tupatilo@gmail.com>
2025-10-24 02:07:51 +0900

    refactoring

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,7 +3,7 @@ import { randomInteger } from '../utils.js'
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-const isEven = number => (number % 2 === 0 ? 'yes' : 'no')
+const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no')
 
 const generateRound = () => {
   const question = randomInteger(1, 101)
--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -3,7 +3,7 @@ import { randomInteger } from '../utils.js'
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
-const isPrime = num => {
+const isPrime = (num) => {
   if (num < 2) return false
   const limit = Math.sqrt(num)
   for (let i = 2; i <= limit; i += 1) {
